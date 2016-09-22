902edf7d502c66d56a626cd1ed411d5ba5a3f524
Dylan <dbarb911@gmail.com>
2016-09-22 15:51:54 -0400

    Created by Coding Ground Client

---
+++ Slot Machine/Game.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Game
+{
+SlotMachine main = new SlotMachine();
+  
+  public void start()
+  {
+    
+    main.getResults(main.results);
+  	main.slotResults();
+    
+  }
+    /*public boolean isBroke(SlotMachine main)
+    {
+        if(main.money<=)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }*/
+
+
+}
